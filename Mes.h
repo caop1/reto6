7110d51ec76bcd7e52f88023b6a7c2512b8dd3ab
David Bocanegra <david@david-VirtualBox.(none)>
2017-05-05 22:02:16 -0500

    ajustes menores al codigo

--- Mes.h
+++ Mes.h
@@ -1,3 +1,12 @@
+/*
+ * Autores:Dayana Herrera Gallego. cod: 1556221
+ * 		   Cristian A. Ospina Pelaez. cod: 1663740
+ *         David Bocanegra Hidalgo. cod: 1663833
+ * Descripcion: Punto 1 / Pactica 3
+ * Fecha de creacion: 05 / 05 / 2017
+ * Fecha de ultima modificacion: 05 / 05 / 2017
+ */
+
 #include <string>
 #include <iostream>
 #ifndef MES_H
--- main2.cpp
+++ main2.cpp
@@ -1,7 +1,7 @@
  /*
  * Autores:Dayana Herrera Gallego. cod: 1556221
  * 		   Cristian A. Ospina Pelaez. cod: 1663740
- *         Juan David Bocanegra Hidalgo. cod: 16
+ *         David Bocanegra Hidalgo. cod: 1663833
  * Descripcion: Punto 2 / Pactica 3
  * Fecha de creacion: 05 / 05 / 2017
  * Fecha de ultima modificacion: 05 / 05 / 2017
